16339748507398bca6dab0a514a526bde78874c7
SBAITIAchraf <achraf.sbaiti@um6p.ma>
2024-04-09 16:37:05 +0000

    Update AdvancedTextAnalyzer.py

--- Classes/AdvancedTextAnalyzer.py
+++ Classes/AdvancedTextAnalyzer.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 import nltk
 from nltk.corpus import stopwords
 import string
+import math
 
 class AdvancedTextAnalyzer():
     def __init__(self,text):
@@ -18,6 +19,42 @@ class AdvancedTextAnalyzer():
     @property
     def sentences(self):
         return self.__sentences
+
+    def calculateIdf(sentences):
+        total = len(sentences)
+
+        # Nembre se phrases contenant un mot
+        freq = {}
+
+        Idf = {}
+
+        nltk.download("stopwords")
+        stp_word = set(stopwords.words("english"))
+
+        # calcule des frequences de chaque mot
+        for i in range(len(sentences)):
+            words = sentences[i].split(" ")
+            for word in words:
+
+                #Enlever la ponctuation
+                if word[-1] in string.punctuation:
+                    word = word[:-1]
+
+                # Rendre tous les lettres en minuscule
+                word = word.lower()
+                
+                if word not in stp_word:
+                    if word not in freq:
+                        freq[word] = 1
+
+                        # Calculer le nembre de phrases contenant word
+                        for j in range(len(sentences)):
+                            if j!=i:
+                                if word in sentences[j]:
+                                    freq[word] +=1
+        for word in freq:
+            Idf[word] = math.log(total/freq[word])
+
 
     def calculate_sentence_similarity(phrase1,phrase2):
         score=0
